e8027245765765b15837d8e40a11d4122b75a3a3
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-01-27 23:25:56 +0700

    update commit

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const { requiresignin } = require("../common-middleware");
 const { signup, signin, signout } = require("../controller/auth");
+const {
+  validationsignin,
+  isRequestValidated,
+  validationsignup,
+} = require("../validator/auth");
 
 const router = express.Router();
 
-router.post("/user/signiup", signup);
+router.post("/user/signup", validationsignup, isRequestValidated, signup);
 
-router.post("/user/signin", signin);
+router.post("/user/signin", validationsignin, isRequestValidated, signin);
 
 router.post("/user/signout", requiresignin, signout);
 
