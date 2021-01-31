5dcf7a999563aa9d5ff939633ab1b092c8a6d18f
Tanatorn-Mokkhao <6106021621041@fitm.kmutnb.ac.th>
2021-01-31 14:50:20 +0700

    update commit

--- src/controller/admin/dashbaord.js
+++ src/controller/admin/dashbaord.js
@@ -48,3 +48,117 @@ exports.queryRevenueByDate = (req, res) => {
     }
   });
 };
+
+// exports.getFilter = (req, res) => {
+//   Order.aggregate([{ $match: { status: "pending" } }]).exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+// exports.getFilter = (req, res) => {
+//   Order.aggregate([
+//     { $match: { status: "pending" } },
+//     { $group: { _id: "$status", totalscore: { $sum: "$totalprice" } } },
+//   ]).exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+// exports.getFilter = (req, res) => {
+//   Order.aggregate([
+//     { $group: { _id: "$status", totalscore: { $sum: "$totalprice" } } },
+//   ]).exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+// exports.getFilter = (req, res) => {
+//   Order.aggregate([
+//     { $group: { _id: "$createdAt", totalscore: { $sum: "$totalprice" } } },
+//   ]).exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+exports.getFilter = (req, res) => {
+  Order.aggregate([
+    { $project: { _id: 0, year: { $year: "$createdAt" } } },
+    { $group: { _id: "$year" } },
+    { $sort: { _id: -1 } },
+  ]).exec((error, filter) => {
+    if (error) return res.status(400).json({ error });
+    if (filter) {
+      return res.status(200).json({ filter });
+    }
+  });
+};
+
+// exports.getPrivotRevenue = (req, res) => {
+//   Order.aggregate([
+//     {
+//       $project: {
+//         _id: 0,
+//         month: { $month: "$createdAt" },
+//         total: "$totalprice",
+//       },
+//     },
+//     { $group: { _id: "$month", totalsale: { $sum: "$total" } } },
+//   ]).exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+// exports.getPrivotRevenue = (req, res) => {
+//   Order.find({ createdAt: { $year: "$createdAt","2021" }})
+//     .exec((error, date) => {
+//     if (error) return res.status(400).json({ error });
+//     if (date) {
+//       return res.status(200).json({ date });
+//     }
+//   });
+// };
+
+exports.getPrivotRevenue = (req, res) => {
+  Order.aggregate([
+    {
+      $project: {
+        _id: 0,
+        year: { $year: "$createdAt" },
+        month: { $month: "$createdAt" },
+        total: "$totalprice",
+      },
+    },
+    {
+      $group: {
+        _id: { year: "$year", month: "$month" },
+        totalsale: { $sum: "$total" },
+      },
+    },
+    {
+      $match: { "_id.year": parseInt(req.body.year) },
+    },
+    {
+      $sort: { "_id.month": 1 },
+    },
+  ]).exec((error, privot) => {
+    if (error) return res.status(400).json({ error });
+    if (privot) {
+      return res.status(200).json({ privot });
+    }
+  });
+};
--- src/router/admin/dashboard.js
+++ src/router/admin/dashboard.js
@@ -1,8 +1,16 @@
 const express = require("express");
-const { queryRevenueByDate } = require("../../controller/admin/dashbaord");
+const {
+  queryRevenueByDate,
+  getFilter,
+  getPrivotRevenue,
+} = require("../../controller/admin/dashbaord");
 
 const router = express.Router();
 
 router.post("/get/dashboard", queryRevenueByDate);
+
+router.post("/get/filter", getFilter);
+
+router.post("/get/privot", getPrivotRevenue);
 
 module.exports = router;
